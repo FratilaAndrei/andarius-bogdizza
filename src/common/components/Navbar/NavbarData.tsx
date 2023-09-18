a46418056d9a37cd35aa4044bc9622c38e0dfec9
Fratila Andrei <Fratila Andrei>
2023-09-18 15:38:11 +0300

    AND-10 extracted Logo into a component

--- src/common/components/Navbar/NavbarData.tsx
+++ src/common/components/Navbar/NavbarData.tsx
@@ -1,4 +1,5 @@
 import properLogo from "../../../utils/properLogo.jpg";
+import NavbarLogo from "./NavbarLogo";
 const NavbarData = () => {
   const NavbarDummyData = [
     { id: 1, label: "HOME", link: "/" },
@@ -10,7 +11,6 @@ const NavbarData = () => {
     { id: 7, label: "CONTACT", link: "/contact" },
   ];
 
-  //TODO Make this getNavbarData into a component
   const getNavbarData = () => {
     return NavbarDummyData.map((option) => (
       <div className="text-center" key={option.id}>
@@ -24,21 +24,7 @@ const NavbarData = () => {
         </a>
         {option.picture && (
           <a href="/">
-            <div className="flex mt-5 ">
-              <div className="w-[15px] h-auto bg-logoColor"></div>
-              <div className="bg-logoColor w-[180px] max-h-[100px] relative">
-                <img
-                  src={option.picture}
-                  alt="ProperLogo"
-                  className="w-full h-full object-cover object-center"
-                />
-                <div className="flex absolute -bottom-13 -right-4  w-[210px] h-[50px]">
-                  <div className="w-1/2  border-r-logoColor border-b-transparent border-r-[105px] border-b-[50px]"></div>
-                  <div className="w-1/2  border-l-logoColor border-b-transparent border-l-[105px] border-b-[50px]"></div>
-                </div>
-              </div>
-              <div className="w-[15px] h-auto bg-logoColor"></div>
-            </div>
+            <NavbarLogo navbarLogo={option.picture} />
           </a>
         )}
       </div>
